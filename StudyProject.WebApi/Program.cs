1e425f39e6ee726a4926549ddc984e81e7297f6c
Александр Бычук <sbychuk645@gmail.com>
2023-06-30 12:46:22 +0300

    remove extra usings

--- StudyProject.WebApi/Program.cs
+++ StudyProject.WebApi/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using StudyProject.Infrastructure.Persistence;
-using StudyProject.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
